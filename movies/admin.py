46cf84467a1687597f05d5294a7649e3b922c486
Ameer Abdulaziz <ameer.abdulaziz93@gmail.com>
2022-11-10 15:43:30 +0200

    Finish scheduling with celery

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SearchTerm, Genre, Movie
+
+
+@admin.register(SearchTerm)
+class SearchTermAdmin(admin.ModelAdmin):
+    list_display = ('id', 'term', 'last_search')
+    list_filter = ('last_search',)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'year',
+        'runtime_minutes',
+        'imdb_id',
+        'plot',
+        'is_full_record',
+    )
+    list_filter = ('is_full_record',)
